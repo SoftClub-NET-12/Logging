1919430a982f5f0c8b4361b9c93b2c68ace78eb4
Ali <133884883+nazarovqurbonali@users.noreply.github.com>
2024-05-23 19:01:24 +0500

    first-commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Services.CateGoryService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(x 
+    => x.UseNpgsql(builder.Configuration.GetConnectionString("Connection")));
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
